e7f0698f7e206deb314141fa71d8a4850b4171c2
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-29 15:16:37 +0300

    add the project

---
+++ CanvasBlogDemo/CanvasBlogDemo/CanvasBlogDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasBlogDemoApp.swift
+//  CanvasBlogDemo
+//
+//  Created by Oleksandr Isaiev on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CanvasBlogDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

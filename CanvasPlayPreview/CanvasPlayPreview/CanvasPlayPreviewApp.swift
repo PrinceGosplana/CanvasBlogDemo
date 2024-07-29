daa1ef71fd34b48bd866f39a49844cea12e888cf
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-29 15:22:33 +0300

    add CanvasPlayPreview

---
+++ CanvasPlayPreview/CanvasPlayPreview/CanvasPlayPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasPlayPreviewApp.swift
+//  CanvasPlayPreview
+//
+//  Created by Oleksandr Isaiev on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CanvasPlayPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
